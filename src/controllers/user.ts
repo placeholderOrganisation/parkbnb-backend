e78c1813358b0cddfa69eb7dd9c0931c546a9642
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-10 23:43:48 -0500

    add route to update user's contact info

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,3 +1,30 @@
 import { Router } from "express";
-
+import { User } from "../models/user-model";
 export const userController = Router();
+
+// Update user route
+userController.put("/complete-sign-up", async (req, res) => {
+
+    const { id, contactInfo } = req.body;
+
+    try {
+        // Find the user by ID
+        const existingUser = await User.findOne({ id });
+
+        if (!existingUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // Update the user's contact information
+        existingUser.contactNumber = contactInfo;
+
+        // Save the updated user
+        await existingUser.save();
+
+        res.status(204).json({ message: "User updated successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   id: String,
   name: String,
-  contact: String,
+  contactNumber: String,
   email: String,
   images: [String],
   rating: {
