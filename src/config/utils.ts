e9c09fa05b7cc6de981e1c8dcd27c7b07510be43
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-10 22:58:28 -0500

    only add user if it has not been saved before

--- src/config/utils.ts
+++ src/config/utils.ts
@@ -2,29 +2,45 @@ import ValidationError from "mongoose";
 import { User } from "../models/user-model";
 
 export const saveUserInDB = async (profile: any, done: any) => {
-
   const userObj = _extractUserData(profile);
 
   const newUser = new User(userObj);
 
-  try {
-    // Validate the user data before saving
-    await newUser.validate();
+  const existingUser = await _checkUserExists(userObj.id);
+  if (existingUser) {
+    console.info("User already exists in the database");
+    return done(null, profile);
+  } else {
+    try {
+      // Validate the user data before saving
+      await newUser.validate();
 
-    // If validation passes, save the user to the database
-    await newUser.save();
-    console.info("saved user to database");
-  } catch (error) {
-    if (error instanceof (ValidationError as any)) {
-      // If validation fails, handle the validation error
-      console.error("Validation error:", error.message);
-    } else {
-      // Handle other errors
-      console.error("Error saving user to database", error);
+      // If validation passes, save the user to the database
+      await newUser.save();
+
+      console.info("saved user to database");
+      return done(null, profile);
+    } catch (error) {
+      if (error instanceof (ValidationError as any)) {
+        // If validation fails, handle the validation error
+        console.error("Validation error:", error.message);
+      } else {
+        // Handle other errors
+        console.error("Error saving user to database", error);
+      }
+      return done(error);
     }
   }
+};
 
-  done(null, profile);
+export const _checkUserExists = async (id: string): Promise<boolean> => {
+  try {
+    const existingUser = await User.findOne({ id });
+    return !!existingUser;
+  } catch (error) {
+    console.error("Error checking user existence:", error);
+    return false;
+  }
 };
 
 const _extractUserData = (profile: any) => {
@@ -34,4 +50,4 @@ const _extractUserData = (profile: any) => {
   const images = profile.photos.map((photo: any) => photo.value);
 
   return { id, name, email, images };
-}
+};
