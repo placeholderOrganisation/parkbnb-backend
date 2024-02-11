a124d02c6fa0c9111a6fe1bdd9f34dc2a22e0451
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-10 22:12:11 -0500

    added mongoose

--- src/config/utils.ts
+++ src/config/utils.ts
@@ -1,5 +1,15 @@
+import { Mongoose } from "mongoose";
+import { getDbConnection } from "./db";
+
+const client: Mongoose = getDbConnection();
+
 export const saveUserInDB = (profile: any, done: any) => {
+  // extract display name
+  // extract email
+  // extract photos
+  // extract id
+
   // insert into user collection/document
-  console.log('profile', profile)
-  done(null, profile)
+  console.log("profile", profile);
+  done(null, profile);
 };
