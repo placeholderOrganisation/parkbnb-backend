f526aa3061761428643215f99ae1b2c9d7f9c1c8
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-10 21:51:38 -0500

    connected mongodb - adding mongoose and creating user schema

---
+++ src/config/db.ts
@@ -0,0 +1,27 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri =
+  "mongodb+srv://admin:1IyDaSFqqN3c0hSs@parkbnb.ermevwo.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+export const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const run = async () => {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
