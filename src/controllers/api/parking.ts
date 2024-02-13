e8479e6095f03e1b4d8de6b847ceeee604d26a37
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-13 00:18:58 -0500

    added parking controller

---
+++ src/controllers/api/parking.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import Parking, { ParkingObject } from "../../models/parking-model";
+
+const router = express.Router();
+
+// Route to update a parking given an ID
+router.put("/parkings/:id", async (req: Request, res: Response) => {
+    try {
+        const parkingId = req.params.id;
+        const parkingData: ParkingObject = req.body;
+        const updatedParking = await Parking.findByIdAndUpdate(parkingId, parkingData, { new: true });
+        if (!updatedParking) {
+            return res.status(404).json({ error: "Parking not found" });
+        }
+        res.status(200).json(updatedParking);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to update parking" });
+    }
+});
+
+router.post("/parkings", async (req: Request, res: Response) => {
+    try {
+        const parkingData: ParkingObject = req.body;
+        const newParking = await Parking.create(parkingData);
+        res.status(201).json(newParking);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to create parking" });
+    }
+});
+
+
+
+export default router;
