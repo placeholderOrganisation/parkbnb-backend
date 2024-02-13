70f34ecf6e9cf8163e826166425d06b675465cdb
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-13 00:21:07 -0500

    clean up s3 utils

--- src/controllers/utils/s3.ts
+++ src/controllers/utils/s3.ts
@@ -1,6 +1,7 @@
 import AWS, { S3 } from "aws-sdk";
+import { returnS3Client } from "../../clients/s3";
 
-const s3: S3 = new AWS.S3();
+const s3: S3 = returnS3Client();
 
 export const uploadToS3 = (file: File): Promise<string> => {
   const params: S3.Types.PutObjectRequest = {
@@ -9,18 +10,18 @@ export const uploadToS3 = (file: File): Promise<string> => {
     Body: file,
   };
 
-return s3
+  return s3
     .upload(params)
     .promise()
     .then((response: S3.ManagedUpload.SendData) => {
-        // Send the file URL using the response or perform any other action
-        console.log("File uploaded successfully. URL:", response.Location);
-        return response.Location;
+      // Send the file URL using the response or perform any other action
+      console.log("File uploaded successfully. URL:", response.Location);
+      return response.Location;
     })
     .catch((error) => {
-        console.error("Error uploading file to S3:", error);
-        const errorMessage = "Failed to upload file to S3";
-        const statusCode = 500;
-        return Promise.reject({ statusCode, errorMessage });
+      console.error("Error uploading file to S3:", error);
+      const errorMessage = "Failed to upload file to S3";
+      const statusCode = 500;
+      return Promise.reject({ statusCode, errorMessage });
     });
 };
