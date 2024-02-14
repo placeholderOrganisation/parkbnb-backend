a7d98d0b906a8d97a48628571451842c31dcd4e6
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-14 17:12:52 -0500

    created s3 util function to upload multiple files

--- src/controllers/utils/s3-utils.ts
+++ src/controllers/utils/s3-utils.ts
@@ -24,7 +24,36 @@ export const uploadFileToS3 = (file: Express.Multer.File): Promise<string> => {
     });
 };
 
-export const downloadFileFromS3 = (key: string): Promise<NodeJS.ReadableStream> => {
+export const uploadFilesToS3 = (
+  files: Express.Multer.File[]
+): Promise<string[]> => {
+  const uploadPromises: Promise<string>[] = files.map((file) => {
+    const params: S3.Types.PutObjectRequest = {
+      Bucket: process.env.BUCKET_NAME as string,
+      Key: file.originalname,
+      Body: file.buffer,
+    };
+
+    return s3
+      .upload(params)
+      .promise()
+      .then((response: S3.ManagedUpload.SendData) => {
+        // Send the file URL using the response or perform any other action
+        // console.log("File uploaded successfully. URL:", response.Location);
+        return response.Location;
+      })
+      .catch((error) => {
+        console.error("Error uploading file to S3:", error);
+        return Promise.reject("Failed to upload file to S3");
+      });
+  });
+
+  return Promise.all(uploadPromises);
+};
+
+export const downloadFileFromS3 = (
+  key: string
+): Promise<NodeJS.ReadableStream> => {
   const params: S3.Types.GetObjectRequest = {
     Bucket: process.env.BUCKET_NAME as string,
     Key: key,
