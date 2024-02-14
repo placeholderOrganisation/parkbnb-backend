3788ee34bf9ccf86a84f7101587f27b0e3a3790f
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-14 17:12:19 -0500

    create s3 route to upload multiple files

--- src/controllers/api/s3-api.ts
+++ src/controllers/api/s3-api.ts
@@ -1,30 +1,28 @@
+import multer from "multer";
 import { Request, Response, Router } from "express";
-const multer = require("multer");
-import { uploadFileToS3, downloadFileFromS3 } from "../utils/s3-utils";
+import {
+  uploadFilesToS3,
+  uploadFileToS3,
+  downloadFileFromS3,
+} from "../utils/s3-utils";
 
 const upload = multer();
 
 export const s3Controller = Router();
 
-s3Controller.get("/images/:key", async (req: Request, res: Response) => {
+s3Controller.get("/get/:key", async (req: Request, res: Response) => {
   try {
     const key = req.params.key;
     const readStream: NodeJS.ReadableStream = await downloadFileFromS3(key);
-    
+
     readStream.pipe(res);
   } catch (error) {
     res.status(500).json({ error: "Failed to download file from S3" });
   }
 });
 
-/**
- * test route for me to test the uploadFileToS3 function
- * once verified the uploafdFileToS3 will be used in the create a new parking route
- *
- * or maybe I should create a sepratae route for uploading files to s3
- */
 s3Controller.post(
-  "/upload",
+  "/upload-single",
   upload.single("file"),
   async (req: Request, res: Response) => {
     try {
@@ -33,6 +31,20 @@ s3Controller.post(
       res.status(200).send(uploadedFileLocation);
     } catch (error) {
       res.status(500).json({ error: "Failed to upload file to S3" });
+    }
+  }
+);
+
+s3Controller.post(
+  "/upload-multiple",
+  upload.array("files", 10),
+  async (req: Request, res: Response) => {
+    try {
+      const files: Express.Multer.File[] = req.files as Express.Multer.File[];
+      const uploadedFileLocations: String[] = await uploadFilesToS3(files);
+      res.status(200).send(uploadedFileLocations);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to upload files to S3" });
     }
   }
 );
