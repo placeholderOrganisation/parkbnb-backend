41096f1ca0806c4a0e646dcbfc2f5f6eb6b9694e
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-02-22 21:22:26 -0500

    change port to 3001

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(
 );
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize(passportConfig));
