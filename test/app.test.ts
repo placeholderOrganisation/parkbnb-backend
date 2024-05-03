4286dbb7a11e8abc478ec65e58755191a6e71eb4
sakshamahluwalia <sakshamahluwalia5@gmail.com>
2024-05-02 22:47:20 -0400

    cleanup

--- test/app.test.ts
+++ test/app.test.ts
@@ -11,7 +11,7 @@ jest.mock("../src/clients/db-client", () => {
 
 describe("GET /test-route", () => {
   it("should return 200 OK", () => {
-      return request(app).get("/test-route")
+      return request(app).get("/sa/test-route")
           .expect(200);
   });
 });
